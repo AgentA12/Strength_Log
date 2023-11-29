70a1748c5889288193961e1808a2b76dc9f96939
Andrew Howran <andrewhowrandev17@gmail.com>
2023-11-29 07:17:26 -0500

    server.js to index.js for vercel

--- server/index.js
+++ server/index.js
@@ -4,23 +4,13 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema/index.js";
 import { authMiddleWare } from "./utils/auth.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
-
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors())
-
-// app.use(express.static(path.join(__dirname, "../client/dist/build")));
-
-// app.get("*", (_, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-// });
+app.use(cors());
 
 const server = new ApolloServer({
   typeDefs,
@@ -36,7 +26,7 @@ const server = new ApolloServer({
 function startApolloServer() {
   db.once("open", async () => {
     await server.start();
-    server.applyMiddleware({ path: "/graphql",app });
+    server.applyMiddleware({ path: "/graphql", app });
     app.listen(PORT, () => {});
   });
 }
